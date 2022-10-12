ab8239af4a7b29e127038ebc610d25eb9a383f66
bks1994K <bks1994@mail.ru>
2022-10-12 15:21:11 +0300

    Add project files.

---
+++ Animals/AnimalsInZoo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class AnimalsInZoo
+    {
+
+        public string Type { get; set; }
+        public string Habitat { get; set; }
+        public int HabitatArea { get; set; }
+        public string[] Food { get; set; }
+        public bool IsPredator { get; set; }
+        public string Sound { get; set; }
+
+        public string Name { get; set; }
+        public double WeightFood { get; set; }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _age = value;
+
+                }
+
+            }
+        }
+
+        private int _age;
+
+        public void DoEat(string food, double weightFood)
+        {
+            for (int i = 0; i < Food.Length; i++)
+            {
+                if (food == Food[i])
+                {
+                    if (weightFood <= WeightFood)
+                    {
+                        Console.WriteLine($"I ate {food}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("I gorged");
+                    }
+                }
+            }
+        }
+
+        public void DoSound(string sound)
+        {
+            if (sound == Sound)
+            {
+                Console.WriteLine($"{sound}");
+            }
+        }
+
+        public void DoPlay()
+        {
+            if (!IsPredator && _age <= 2)
+            {
+                Console.WriteLine($"{Type} play with you");
+            }
+            else if (IsPredator)
+            {
+                Console.WriteLine($"Play with {Type} is dangerous");
+            }
+        }
+
+    }
+}
---
+++ Animals/Program.cs
@@ -0,0 +1,25 @@
+﻿using Animals;
+
+AnimalsInZoo elephant = new AnimalsInZoo();
+elephant.Type = "elephant";
+elephant.Food = new string[] { "grass", "leaves" }; 
+elephant.WeightFood = 100;
+
+elephant.DoEat("grass", 90);
+elephant.Age = 1;
+elephant.IsPredator = false;
+Console.WriteLine(elephant.Age);
+elephant.DoPlay();
+
+
+AnimalsInZoo tiger = new AnimalsInZoo();
+tiger.Type = "tiger";
+tiger.Food = new string[] { "meat" };
+tiger.WeightFood = 80;
+tiger.Sound = "Rrrrrrr";
+tiger.IsPredator = true;
+tiger.Age = 1;
+
+tiger.DoEat("meat", 100);
+tiger.DoSound("Rrrrrrr");
+tiger.DoPlay();
